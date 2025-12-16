09b03713accb8cb0017527558f0a59d488fa0197
Shubham Sharma <codewithshubh313@gmail.com>
2025-12-16 10:18:33 +0530

    Update _app.js

--- Coaching-System/client/pages/_app.js
+++ Coaching-System/client/pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/globals.css'
 import Script from "next/script";
 import { useRouter } from "next/router";
-import protectedRoute from "../components/protectedRoute";
+import protectedRoute from "../components/protectedRoute.js";
 
 const openRoutes = ["/login", "/signup"]; // not protected
 
@@ -28,5 +28,6 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
 
 
